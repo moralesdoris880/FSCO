8ba2b6dfc55776d4bfc67a679b769d99ba32fac2
Doris Morales <moralesdoris880@gmail.com>
2022-01-18 19:44:42 -0500

    Init Repo

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App(){
+    return(
+        <div>
+        //Character Info Sheet
+        //Store Map
+        //Store Service Tab
+        </div>
+    )
+}
+
+export default App;
