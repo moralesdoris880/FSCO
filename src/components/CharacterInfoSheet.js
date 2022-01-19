42ce9f43f80d232ea8d7276ab6eb71f7b9df8b26
Doris Morales <moralesdoris880@gmail.com>
2022-01-19 17:51:08 -0500

    Working on Character Sheet Component

--- src/components/CharacterInfoSheet.js
+++ src/components/CharacterInfoSheet.js
@@ -3,7 +3,24 @@ import React from "react";
 function CharacterInfoSheet(){
     return(
         <div id="charactersheet">
-            Character Sheet
+            <div id="innercharactersheet">
+                <h1 id="name">NAME</h1> 
+                <div id="statslist">
+                    <div className="statsholder">
+                        <p className="stats">HP</p>
+                        <p>100</p>
+                    </div>
+                    <div className="statsholder">
+                        <p className="stats">GP</p>
+                        <p>100</p>
+                    </div>
+                    <div className="statsholder">
+                        <p className="stats">COIN</p>
+                        <p>20.08</p>
+                    </div>
+                    <p className="snack">SNACK</p>
+                </div>
+            </div>
         </div>
     )
 }
